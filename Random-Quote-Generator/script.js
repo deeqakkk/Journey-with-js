06f077033165944743f43d6eac29586336814033
jain <jainpatel25@gmail.com>
2022-10-11 10:06:24 +0530

    modified projet random qoute generator

--- Random-Quote-Generator/script.js
+++ Random-Quote-Generator/script.js
@@ -162,14 +162,12 @@ var quotes = [
 	source: "Aristotle"
 	}
 ];
-
 //Function to randomly select a quote value and return a random quote object from the quotes array
 function getRandomQuote () {
 	var randomNumber = Math.floor(Math.random() * (quotes.length));
 	var randomQuote = quotes[randomNumber];
 	return randomQuote;
 }	
-
 //Function to select random rgb color value
 function getRandomColor () {
 	var red = Math.floor(Math.random() * 256 );
@@ -178,7 +176,6 @@ function getRandomColor () {
 	 var randomColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 	return randomColor;
 }
-
 //Function to call the getRandomQuote function and stores the returned quote object in a variable
 //Constructs a string containing the different properties of the quote object 
 function printQuote () {
@@ -193,13 +190,10 @@ function printQuote () {
 					//assigns random color value to document background color 
 					document.body.style.backgroundColor = getRandomColor ();
 }
-
 //Quote automatically refreshes every 15 seconds
 window.setInterval(function(){
 printQuote ();
 }, 20000);
-
-//Event listener on LoadQuote button to generate new quote		
 document.getElementById("loadQuote").addEventListener("click", printQuote, false);
 
 
