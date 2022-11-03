319fc9dc155075c96e0078958d1c87103a8f3b9b
Restyled.io <commits@restyled.io>
2022-11-03 18:31:49 +0000

    Restyled by clang-format

--- 29-BMI Calculator/script.js
+++ 29-BMI Calculator/script.js
@@ -1,20 +1,17 @@
 function calculate() {
-    var height =
-   parseFloat(document.getElementById("height").value);
-    var weight =
-   parseFloat(document.getElementById("weight").value);
-    var bmi = weight /(height*height);
-    if(bmi < 18.5) {
+  var height = parseFloat(document.getElementById("height").value);
+  var weight = parseFloat(document.getElementById("weight").value);
+  var bmi = weight / (height * height);
+  if (bmi < 18.5) {
     document.getElementById("result").innerHTML =
-   "Underweight : " + bmi.toFixed(2);
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        "Underweight : " + bmi.toFixed(2);
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
     document.getElementById("result").innerHTML =
-   "Healthy Weight : " + bmi.toFixed(2);
-    } else if (bmi >= 25.0 && bmi <= 29.9) {
+        "Healthy Weight : " + bmi.toFixed(2);
+  } else if (bmi >= 25.0 && bmi <= 29.9) {
     document.getElementById("result").innerHTML =
-   "Overweight : " + bmi.toFixed(2);
-    } else if (bmi >= 30.0) {
-    document.getElementById("result").innerHTML =
-   "Obesity : " + bmi.toFixed(2);
-    }
-    }
+        "Overweight : " + bmi.toFixed(2);
+  } else if (bmi >= 30.0) {
+    document.getElementById("result").innerHTML = "Obesity : " + bmi.toFixed(2);
+  }
+}
