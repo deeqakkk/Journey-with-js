323b4dafb44583336699cbcf63f764111aa9da38
Restyled.io <commits@restyled.io>
2022-11-03 18:31:51 +0000

    Restyled by prettier

--- 29-BMI Calculator/script.js
+++ 29-BMI Calculator/script.js
@@ -4,13 +4,13 @@ function calculate() {
   var bmi = weight / (height * height);
   if (bmi < 18.5) {
     document.getElementById("result").innerHTML =
-        "Underweight : " + bmi.toFixed(2);
+      "Underweight : " + bmi.toFixed(2);
   } else if (bmi >= 18.5 && bmi <= 24.9) {
     document.getElementById("result").innerHTML =
-        "Healthy Weight : " + bmi.toFixed(2);
+      "Healthy Weight : " + bmi.toFixed(2);
   } else if (bmi >= 25.0 && bmi <= 29.9) {
     document.getElementById("result").innerHTML =
-        "Overweight : " + bmi.toFixed(2);
+      "Overweight : " + bmi.toFixed(2);
   } else if (bmi >= 30.0) {
     document.getElementById("result").innerHTML = "Obesity : " + bmi.toFixed(2);
   }
